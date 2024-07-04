f8bb46fb73c1db4fa8e46d36671af0e6a3274dfb
Paulo Vitor <angelopaulovitor@gmail.com>
2024-07-04 15:51:46 -0300

    commit final

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import br.com.dio.desafio.dominio.Conteudo;
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
@@ -27,10 +28,37 @@ public class Main {
 		mentoria1.setData(LocalDate.parse("30-07-2024",DateTimeFormatter.ofPattern("dd-MM-yyyy")));
 		
 		
-		System.out.println(curso1);
-		System.out.println(curso2);
-		System.out.println(mentoria1);
+		Bootcamp bootcamp1 = new Bootcamp();
+		
+		bootcamp1.setNome("Bootcamp Java Developer");
+		bootcamp1.setDescricao("Descrição Bootcamp Java Dev");
+		bootcamp1.getConteudos().add(curso1);
+		bootcamp1.getConteudos().add(curso2);
+		bootcamp1.getConteudos().add(mentoria1);
+		
+		
+		Dev devCamila = new Dev();
+		Dev devPaulo = new Dev();
+		
+		devCamila.setNome("Camila");
+		devPaulo.setNome("Paulo");
+		devCamila.inscreverBootcamp(bootcamp1);
+		devPaulo.inscreverBootcamp(bootcamp1);
+		
+		System.out.println("Conteudos inscritos de camila: " + devCamila.getConteudosInscritos());
+		System.out.println("Conteudos inscritos de Paulo: " + devPaulo.getConteudosInscritos());
+		
+		
+		System.out.println("Alunos inscritos no bootcamp: " + bootcamp1.getDevsInscritos());
+		
+		
+		devPaulo.progredir();
+		
+		System.out.println("Conteúdos concluídos de Camila: " + devCamila.getConteudosConcluidos());
+		System.out.println("Conteúdos concluídos de Paulo: " + devPaulo.getConteudosConcluidos());
 
+		System.out.println("XP total de Camila: " + devCamila.calcularTotalXp());
+		System.out.println("XP total de Paulo: " + devPaulo.calcularTotalXp());
 	}
 
 }
--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -1,9 +1,73 @@
 package br.com.dio.desafio.dominio;
 
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
 private String nome;
-private Set<Conteudo> conteudosInscritos;
-private Set<Conteudo> conteudosConcluidos;
+private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+
+public void inscreverBootcamp(Bootcamp bootcamp) {
+	conteudosInscritos.addAll(bootcamp.getConteudos());
+	bootcamp.getDevsInscritos().add(this);
+}
+
+public void progredir() {
+	Optional<Conteudo> conteudo = conteudosInscritos.stream().findFirst();
+	if(conteudo.isPresent()) {
+		conteudosConcluidos.add(conteudo.get());
+		conteudosInscritos.remove(conteudo.get());
+	} else {
+		System.err.println("Você não está matriculado em nenhum conteúdo");
+	}
+}
+
+public double calcularTotalXp() {
+	return conteudosConcluidos.stream().mapToDouble(c -> c.calcularXp()).sum();
+}
+
+public String getNome() {
+	return nome;
+}
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+
+public Set<Conteudo> getConteudosInscritos() {
+	return conteudosInscritos;
+}
+
+public Set<Conteudo> getConteudosConcluidos() {
+	return conteudosConcluidos;
+}
+
+
+@Override
+public int hashCode() {
+	return Objects.hash(nome);
+}
+
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Dev other = (Dev) obj;
+	return Objects.equals(nome, other.nome);
+}
+
+@Override
+public String toString() {
+	return nome;
+}
+
 }
